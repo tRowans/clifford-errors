bea834bd6b8d09a8eab4fb823f0de2bc7a4c6f45
Michael Vasmer <mikevasmer@gmail.com>
2022-01-05 14:34:51 -0500

    Script and input for compute canada

---
+++ input/gen_input.py
@@ -0,0 +1,25 @@
+import csv
+import subprocess
+import numpy as np
+from datetime import datetime
+
+Lmin = 6
+Lmax = 10
+Ls = range(Lmin, Lmax + 1, 2)
+ps = np.linspace(0.05, 1.05, 10)
+runs = int(1e2)
+job = 0
+git_hash = subprocess.check_output(['git', 'rev-parse',  'HEAD']).strip()
+link = 1
+today = datetime.today().strftime('%Y-%m-%d')
+max_iter = 10
+osd_order = 6
+osd_method = 'osd_cs'
+
+with open(f'{today}.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(['L', 'p', 'q', 'runs', 'linking', 'max_iter', 'osd_order', 'osd_method', 'job_number', 'git hash (metadata)'])
+    for L in Ls:
+        for p in ps:
+            writer.writerow([L, p, p, runs, link, max_iter, osd_order, osd_method, job, git_hash])
+            job += 1
