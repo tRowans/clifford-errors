9371a4f8b544778f7a3924895268f0f3426d2070
MikeVasmer <michael_vasmer@hotmail.co.uk>
2022-01-07 17:43:23 -0500

    Cluster version of job and input gen files

--- input/gen_input.py
+++ input/gen_input.py
@@ -6,17 +6,18 @@ from datetime import datetime
 Lmin = 6
 Lmax = 12
 Ls = range(Lmin, Lmax + 1, 2)
-ps = np.linspace(0.005, 0.014, 10)
+# ps = np.concatenate((np.linspace(0.001, 0.004, 4), np.linspace(0.015, 0.024, 10)), axis=None)
+ps = np.linspace(0.001, 0.035, 18)
 runs = int(1e4)
 job = 0
 git_hash = subprocess.check_output(['git', 'rev-parse',  'HEAD']).strip()
-link = 1
+link = 2
 today = datetime.today().strftime('%Y-%m-%d')
-max_iter = 10
-osd_order = 6
+max_iter = 50
+osd_order = 10
 osd_method = 'osd_cs'
 
-with open(f'{today}.csv', 'w') as csv_file:
+with open(f'{today}b.csv', 'w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
     writer.writerow(['L', 'p', 'q', 'runs', 'linking', 'max_iter', 'osd_order', 'osd_method', 'job_number', 'git hash (metadata)'])
     for L in Ls:
